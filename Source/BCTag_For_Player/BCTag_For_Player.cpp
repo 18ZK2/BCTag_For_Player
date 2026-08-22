849ecd9a6217e463eb7a03966ad015208529b9fc
K2 <kawabe4545@gmail.com>
2026-08-22 23:59:08 +0900

    delete catch

--- Source/BCTag_For_Player/BCTag_For_Player.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BCTag_For_Player.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BCTag_For_Player, "BCTag_For_Player" );
