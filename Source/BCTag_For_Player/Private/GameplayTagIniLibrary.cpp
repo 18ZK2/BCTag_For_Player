22aa0a608338753503dbb0579ac8700727949b36
K2 <kawabe4545@gmail.com>
2026-09-20 23:32:13 +0900

    ミッションツリー完成？

--- Source/BCTag_For_Player/Private/GameplayTagIniLibrary.cpp
+++ Source/BCTag_For_Player/Private/GameplayTagIniLibrary.cpp
@@ -4,9 +4,7 @@
 #include "Misc/Paths.h"
 #include "GameplayTagsManager.h"
 
-TArray<FGameplayTag> UGameplayTagIniLibrary::GetGameplayTagsFromIni(
-	const FString& IniFilePath
-)
+TArray<FGameplayTag> UGameplayTagIniLibrary::GetGameplayTagsFromIni(const FString& IniFilePath)
 {
 	TArray<FGameplayTag> Result;
 
@@ -101,4 +99,127 @@ TArray<FGameplayTag> UGameplayTagIniLibrary::GetGameplayTagsFromIni(
 	}
 
 	return Result;
+}
+
+int32 UGameplayTagIniLibrary::GetBranchWidth(const TArray<FMissionNode>& Nodes, int32 NodeIndex)
+{
+	const FMissionNode& Node = Nodes[NodeIndex];
+
+	// 子がない
+	if (Node.Children.Num() == 0)
+	{
+		return 1;
+	}
+
+	// 子が1つ
+	if (Node.Children.Num() == 1)
+	{
+		return GetBranchWidth(
+			Nodes,
+			Node.Children[0]
+		);
+	}
+
+	// 子が複数
+	int32 Width = 0;
+
+	for (int32 ChildIndex : Node.Children)
+	{
+		Width += GetBranchWidth(
+			Nodes,
+			ChildIndex
+		);
+	}
+
+	return Width;
+}
+
+void UGameplayTagIniLibrary::SetXPosition(TArray<FMissionNode>& Nodes, int32 NodeIndex, float x, float stepX)
+{
+	FMissionNode& Node = Nodes[NodeIndex];
+	Node.Position.X = x;
+	// --------------------------------
+	// 子がない場合
+	// --------------------------------
+	if (Node.Children.Num() == 0)
+	{
+		return;
+	}
+
+	// --------------------------------
+	// 子が1つ
+	// --------------------------------
+
+	if (Node.Children.Num() == 1)
+	{
+		SetXPosition(Nodes, Node.Children[0], x, stepX);
+		return;
+	}
+
+	// --------------------------------
+	// 子が複数 → 分岐
+	// --------------------------------
+
+	const int32 ChildCount = Node.Children.Num();
+
+	float CurrentX = x;
+
+	for (int32 i = 0; i < ChildCount; ++i)
+	{
+		SetXPosition(Nodes, Node.Children[i], CurrentX, stepX);
+		CurrentX += GetBranchWidth(Nodes, Node.Children[i]) * stepX;
+	}
+}
+
+
+
+TArray<FMissionNode> UGameplayTagIniLibrary::CreateMissionTree(const TArray<FGameplayTag>& GameplayTags ,FVector2D step) {
+
+	TArray<FMissionNode> Nodes;
+
+	// 1. 全GameplayTagをNode化
+	for (const FGameplayTag& Tag : GameplayTags)
+	{
+		FMissionNode Node;
+		Node.Tag = Tag;
+		Node.Position = FVector2D::ZeroVector;
+
+		Nodes.Add(Node);
+	}
+
+	// 2. 親子関係を構築
+	for (int32 ChildIndex = 0; ChildIndex < Nodes.Num(); ++ChildIndex)
+	{
+		const FGameplayTag ChildTag = Nodes[ChildIndex].Tag;
+
+		FGameplayTag ParentTag = ChildTag.RequestDirectParent();
+
+		if (!ParentTag.IsValid())
+		{
+			continue;
+		}
+
+		// 親を検索
+		for (int32 ParentIndex = 0; ParentIndex < Nodes.Num(); ++ParentIndex)
+		{
+			if (Nodes[ParentIndex].Tag == ParentTag)
+			{
+				Nodes[ParentIndex].Children.Add(ChildIndex);
+				break;
+			}
+		}
+	}
+
+	//set depth
+	for (FMissionNode& Node : Nodes)
+	{
+		FString tagstr = Node.Tag.GetTagName().ToString();
+		TArray<FString> tagarr;
+		tagstr.ParseIntoArray(tagarr, TEXT("."), true);
+		int32 depth = tagarr.Num() - 1;
+		Node.Position.Y = depth * step.Y;
+
+	}
+	SetXPosition(Nodes, 0, 0.0f, step.X);
+	return Nodes;
 }
--- Source/BCTag_For_Player/Public/GameplayTagIniLibrary.h
+++ Source/BCTag_For_Player/Public/GameplayTagIniLibrary.h
@@ -1,9 +1,24 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "GameplayTagContainer.h"
 #include "GameplayTagIniLibrary.generated.h"
+
+#define VTAG "UE C++ book"
+#define FTAG "Gameplay Tags"
+
+USTRUCT(BlueprintType)
+struct FMissionNode
+{
+	GENERATED_BODY()
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = VTAG)
+	FGameplayTag Tag;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = VTAG)
+	TArray<int32> Children;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = VTAG)
+	FVector2D Position;
+
+};
 
 UCLASS()
 class BCTAG_FOR_PLAYER_API UGameplayTagIniLibrary : public UBlueprintFunctionLibrary
@@ -12,8 +27,20 @@ class BCTAG_FOR_PLAYER_API UGameplayTagIniLibrary : public UBlueprintFunctionLib
 
 public:
 
-	UFUNCTION(BlueprintCallable, Category = "Gameplay Tags",BlueprintPure)
-	static TArray<FGameplayTag> GetGameplayTagsFromIni(
-		const FString& IniFilePath
+	UFUNCTION(BlueprintCallable, Category = FTAG,BlueprintPure)
+	static TArray<FGameplayTag> GetGameplayTagsFromIni(const FString& IniFilePath);
+
+	UFUNCTION(BlueprintCallable, Category = FTAG,BlueprintPure)
+	static TArray<FMissionNode> CreateMissionTree(const TArray<FGameplayTag>& GameplayTags, FVector2D step);
+
+private:
+	UFUNCTION(BlueprintCallable, Category = FTAG, BlueprintPure)
+	static void SetXPosition(TArray<FMissionNode>& Nodes, int32 NodeIndex, float x, float stepX);
+
+	UFUNCTION(BlueprintCallable, Category = FTAG, BlueprintPure)
+	static int32 GetBranchWidth(
+		const TArray<FMissionNode>& Nodes,
+		int32 NodeIndex
 	);
+
 };
